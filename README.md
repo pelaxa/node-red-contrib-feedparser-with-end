9e3150cb275268274abc4685ec170c8fa8985963
Korey Sedaghatian <4706332+pelaxa@users.noreply.github.com>
2025-04-26 15:58:08 -0400

    updated module name and added extra output on end

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-node-red-contrib-feedparser-extended
+node-red-contrib-feedparser-with-end
 ========================
 
-A modified version of [node-red-node-feedparser2](https://github.com/RayPS/node-red-contrib-feedparser-2) 
+A modified version of [node-red-node-feedparser-extended](https://github.com/arossmann/node-red-contrib-feedparser-extended) 
 
 Extended the input for injection, not only for timing, but also receiving the url as payload from previous step. 
 This is good for debugging the flow or a flexible interval.
+
+This node has two outputs:  
+- The first is only called once the feed articles have been looped.  The payload in this case is the timestamp of the most recent article.
+- The second is called for each article in the feed.
 
 ![](screenshot.png)
 
@@ -14,4 +18,4 @@ Install
 
 Run the following command in your Node-RED user directory - typically `~/.node-red`
 
-        npm install node-red-contrib-feedparser-extended
+        npm install node-red-contrib-feedparser-with-end
--- feedparse.js
+++ feedparse.js
@@ -1,67 +1,77 @@
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     "use strict";
     var FeedParser = require("feedparser");
     var request = require("request");
     var url = require('url');
 
     function FeedParseNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.url = n.url;
         var node = this;
         this.seen = {};
         var parsedUrl = url.parse(this.url);
         if (!(parsedUrl.host || (parsedUrl.hostname && parsedUrl.port)) && !parsedUrl.isUnix) {
-            this.error(RED._("feedparse-extended.errors.invalidurl"));
+            this.error(RED._("feedparse-with-end.errors.invalidurl"));
         }
         else {
-            var getFeed = function(msg) {
+            var getFeed = function (msg) {
                 var feed_url;
-                if(msg.payload != null){
+                if (msg.payload != null) {
                     feed_url = msg.payload;
                 } else {
                     feed_url = node.url;
                 }
-                var req = request(feed_url, {timeout:10000, pool:false});
+                var req = request(feed_url, { timeout: 10000, pool: false });
                 //req.setMaxListeners(50);
                 req.setHeader('user-agent', 'Mozilla/5.0 (Node-RED)');
                 req.setHeader('accept', 'text/html,application/xhtml+xml');
 
                 var feedparser = new FeedParser();
 
-                req.on('error', function(err) { node.error(err); });
+                req.on('error', function (err) { node.error(err); });
 
-                req.on('response', function(res) {
-                    if (res.statusCode != 200) { node.warn(RED._("feedparse-extended.errors.badstatuscode")+" "+res.statusCode); }
+                req.on('response', function (res) {
+                    if (res.statusCode != 200) { node.warn(RED._("feedparse-with-end.errors.badstatuscode") + " " + res.statusCode); }
                     else { res.pipe(feedparser); }
                 });
 
-                feedparser.on('error', function(error) { node.error(error); });
+                feedparser.on('error', function (error) { node.error(error); });
 
                 feedparser.on('readable', function () {
                     var stream = this, article;
                     while (article = stream.read()) {  // jshint ignore:line
-                        if (!(article.guid in node.seen) || ( node.seen[article.guid] !== 0 && node.seen[article.guid] != article.date.getTime())) {
-                            node.seen[article.guid] = article.date?article.date.getTime():0;
-                            
-                            msg.topic = article.origlink || article.link
-                            msg.payload = article.description
-                            msg.article = article
-                            
-                            node.send(msg);
+                        if (!(article.guid in node.seen) || node.seen[article.guid] != article.date.getTime()) {
+                            // Keep track of the latest article
+                            if (!node.latestArticleTS || node.latestArticleTS < article.date.getTime()) {
+                                node.latestArticleTS = article.date.getTime();
+                            }
+                            node.seen[article.guid] = article.date ? article.date.getTime() : 0;
+
+                            msg.topic = article.origlink || article.link;
+                            msg.payload = article.description;
+                            msg.article = article;
+
+                            node.send([null, msg]);
                         }
                     }
                 });
 
-                feedparser.on('meta', function (meta) {});
-                feedparser.on('end', function () {});
+                feedparser.on('meta', function (meta) { });
+                feedparser.on('end', function () {
+                    delete msg.topic;
+                    delete msg.article
+                    // Return the latest article's timestamp
+                    msg.payload = node.latestArticleTS;
+                    node.send(msg);
+                });
             };
         }
 
-        this.on("input", function(msg) {
+        this.on("input", function (msg) {
             getFeed(msg);
         });
     }
 
-    RED.nodes.registerType("feedparse-extended",FeedParseNode);
+    RED.nodes.registerType("feedparse-with-end", FeedParseNode);
 }
